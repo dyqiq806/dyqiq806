15c53a19858cb231673fe7a5615c39036676b22c
dyqiq806@icloud.com <dyqiq806@icloud.com>
2020-10-25 00:36:38 +0800

    homework

---
+++ homework/作业3/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  作业3
+//
+//  Created by dyqiq806 on 2020/10/24.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("    name  number    math  english  computer\n");
+    printf("zhanghua    9901   80.50     87.0        80\n");
+    printf("    lina    9902  100.00     80.0        90\n");
+    printf("wanggang    9903   87.00     76.0        78\n");
+    return 0;
+}
